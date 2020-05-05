d8b84b3aa8e2e4b4f307c7aa85c376a6d9f1dc6d
Christopher Marti <chrisjm093@gmail.com>
2020-05-04 22:20:11 -0700

    Search functionality is working by search term and by begin and end date. HTML formatting changed date form to a drop down. Formatting needed in HTML to accommodate form changes.

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
  // Build a query url to get data from
  var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
  var apiKey = '&api-key=HyveNee7ZsA36O8F8MSUJ1LSgvC0UEk0'
-var beginDate = 'begin_date^\d{8}$'
-var endDate = 'end_date^\d{8}$'
+var beginDate = '$begin_date^\d{8}$'
+var endDate = '$end_date^\d{8}$'
 var query = '?q='
 var numberRecords = $('#numberRecords');
 
@@ -13,26 +13,27 @@ var numberRecords = $('#numberRecords');
 //$("#searchTerm").on("click", function(e) {
     $('#search').on('click', function(event){ 
         
-        
+        event.preventDefault()
         
       
     //event.preventDefault();
     var numberRecords = $('#numberRecords').text();
     var searchTerm = $('#searchTerm' ).val();
-    var startYear = $('#startYear').text();
-    var enddYear = $('#endYear').text();
-    
-    console.log(searchTerm)
+    var startYear = $('#start-year').val();
+    var endYear = $('#end-year').val();
+    var fullURL = "";
    
    // Fully constructed URL based on search
-    var fullURL = queryURL + query + searchTerm + apiKey;
+    var searchURL = query + searchTerm ;
+    var startSearchYear = beginDate + startYear + '0101';
+    var endSearchYear = endDate + endYear +'1231';
 
-    console.log(fullURL)
-    // if ( startYear != null ) {
-
-    //     fullURL = fullURL + "&begin_date="  + startYear + "0101";
-
-    // }
+    if (startYear) {
+        fullURL = queryURL + searchURL + startSearchYear + endSearchYear + apiKey;
+    } else {
+        fullURL = queryURL + searchURL + apiKey;
+    }
+console.log (fullURL)
 
     $.ajax({
         url: fullURL,
@@ -42,9 +43,6 @@ var numberRecords = $('#numberRecords');
         console.log( response )
     })
 
-   // $( '' )
-    
-
-   // queryURL + query + apiKey
+  
 });
 
